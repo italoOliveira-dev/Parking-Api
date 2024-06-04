49bf8042888a28226f2d7dc9d717f8d752bc924c
Italo Oliveira <italooliveira-dev@hotmail.com>
2024-06-04 20:17:02 -0300

    feat: :sparkles: add user entity.

---
+++ src/main/java/br/com/projeto/parking_api/models/entity/Usuario.java
@@ -0,0 +1,77 @@
+package br.com.projeto.parking_api.models.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Getter @Setter @NoArgsConstructor
+public class Usuario implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username",nullable = false, unique = true, length = 100)
+    private String username;
+    @Column(name = "password" ,nullable = false, length = 200)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role" ,nullable = false, length = 25)
+    private Role role;
+
+    @Column(name = "data_cricacao")
+    private LocalDateTime dataCriacao;
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+
+    @Column(name = "criado_por")
+    private String criadoPor;
+    @Column(name = "modificado_por")
+    private String modificadoPor;
+
+    public enum Role {
+        ROLE_ADMIN, ROLE_CLIENTE
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario [id=" + id + "]";
+    }
+
+    
+}
